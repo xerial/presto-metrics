fa5bcae242aa6e04fbc610999839c2484ba743ae
Taro L. Saito <leo@xerial.org>
2014-08-26 14:38:20 +0900

    Add Metrics::Client class

--- lib/presto/metrics.rb
+++ lib/presto/metrics.rb
@@ -1,7 +1,22 @@
 require "presto/metrics/version"
+require 'httparty'
+require 'json'
 
 module Presto
   module Metrics
-    # Your code goes here...
+  	class Client 
+	  	def initialize(opts={})
+ 			@host = opts[:host] || "localhost"
+  			@port = opts[:port] || "8080"
+		 	@endpoint = opts[:endpoint] || "http://#{@host}:#{@port}"
+		end
+
+		def get(mbean)
+			resp = HTTParty.get("#{@endpoint}/v1/jmx/mbean/#{mbean}")
+			json = JSON.parse(resp.body)
+			json
+	  	end
+  	end 
   end
 end
+
