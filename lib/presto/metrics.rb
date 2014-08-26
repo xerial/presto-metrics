1969aaa9b849816d78e5776a2ded2817ba694ae8
Taro L. Saito <leo@xerial.org>
2014-08-26 15:47:22 +0900

    Add methods for collecting Presto metrics

--- lib/presto/metrics.rb
+++ lib/presto/metrics.rb
@@ -13,9 +13,52 @@ module Presto
 
 		def get(mbean)
 			resp = HTTParty.get("#{@endpoint}/v1/jmx/mbean/#{mbean}")
-			json = JSON.parse(resp.body)
-			json
+			JSON.parse(resp.body)
 	  	end
+
+	  	def get_attributes(mbean) 
+			json = get(mbean)
+			json['attributes'] || []
+	  	end
+
+	  	def get_attribute(mbean, attr_name) 
+			get_attributes(mbean).find {|obj| obj['name'] == attr_name } || {}
+	  	end
+
+	  	def get_metrics(mbean) 
+	  		kv = Hash.new
+	  		get_attributes(mbean)
+	  		 	.reject {|attr| attr['name'].nil? || attr['value'].nil? }
+	  		 	.each { |attr|
+	  		 		kv[attr['name']] = attr['value']
+	  		  	}
+	  		kv
+	  	end
+
+	  	def memory_usage_metrics
+	  		get_metrics("java.lang:type=Memory")
+	  	end
+
+	  	def query_manager_metrics
+	  		get_metrics("com.facebook.presto.execution:name=QueryManager")
+	  	end
+
+	  	def query_execution_metrics
+	  		get_metrics("com.facebook.presto.execution:name=QueryExecution")
+	  	end
+
+	  	def node_scheduler_metrics
+	  		get_metrics("com.facebook.presto.execution:name=NodeScheduler")
+	  	end
+
+	  	def task_executor_metrics
+	  		get_metrics("com.facebook.presto.execution:name=TaskExecutor")
+	  	end
+
+	  	def task_manager_metrics
+	  		get_metrics("com.facebook.presto.execution:name=TaskManager")
+	  	end
+
   	end 
   end
 end
