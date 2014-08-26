fa58b39b18581a7a730439c18e16b57e8e349936
Taro L. Saito <leo@xerial.org>
2014-08-26 16:42:39 +0900

    Use underscore case (symbol) for returned metrics. Accept singleton for selecting an attribute

--- lib/presto/metrics.rb
+++ lib/presto/metrics.rb
@@ -4,17 +4,25 @@ require 'json'
 
 module Presto
   module Metrics
+
   	class Client 
+		private :map_to_canonical_name, :to_canonical_name, :underscore
+
 	  	def initialize(opts={})
  			@host = opts[:host] || "localhost"
   			@port = opts[:port] || "8080"
 		 	@endpoint = opts[:endpoint] || "http://#{@host}:#{@port}"
+		 	@caml_case = opts[:caml_case] || false
 		end
 
 		def get(mbean)
-			resp = HTTParty.get("#{@endpoint}/v1/jmx/mbean/#{mbean}")
-			JSON.parse(resp.body)
+			JSON.parse(get_json(mbean))
 	  	end
+
+	  	def get_json(mbean) 
+			resp = HTTParty.get("#{@endpoint}/v1/jmx/mbean/#{mbean}")
+			resp.body
+	  	end 
 
 	  	def get_attributes(mbean) 
 			json = get(mbean)
@@ -25,50 +33,74 @@ module Presto
 			get_attributes(mbean).find {|obj| obj['name'] == attr_name } || {}
 	  	end
 
-	  	def get_metrics(mbean) 
+
+
+	  	def map_to_canonical_name(name_arr) 
+	  		name_arr.map{|name| to_canonical_name(name)	}
+		end
+
+		def to_canonical_name(name) 
+	  		name.to_s.downcase.gsub(/_/, "")
+	  	end
+
+  	    def underscore(str)
+	    	str.gsub(/::/, '/').
+		    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+	    	gsub(/([a-z\d])([A-Z])/,'\1_\2').
+		    tr("-", "_").
+		    downcase
+		end
+
+	  	def get_metrics(mbean, target_attr=[]) 
 	  		kv = Hash.new
+	  		arr = target_attr.kind_of?(Array) ? target_attr : [target_attr]
+	  		c_target_attr = map_to_canonical_name(arr).to_set
 	  		get_attributes(mbean)
 	  		 	.reject {|attr| attr['name'].nil? || attr['value'].nil? }
-	  		 	.each { |attr|
-	  		 		kv[attr['name']] = attr['value']
-	  		  	}
+	  		 	.each {|attr| 
+	  		 		c_name = to_canonical_name(attr['name'])
+	  		 		if c_target_attr.empty? || c_target_attr.include?(c_name)
+	  		 			key = @caml_case ? attr['name'] : underscore(attr['name']).to_sym 
+	  		 			kv[key] = attr['value'] 
+	  		 		end
+	  		 	}
 	  		kv
 	  	end
 
-	  	def memory_usage_metrics
-	  		get_metrics("java.lang:type=Memory")
+	  	def memory_usage_metrics(target_attr=[])
+	  		get_metrics("java.lang:type=Memory", target_attr)
 	  	end
 
-	  	def gc_cms_metrics
-	  		get_metrics("java.lang:type=GarbageCollector,name=ConcurrentMarkSweep")
+	  	def gc_cms_metrics(target_attr=[])
+	  		get_metrics("java.lang:type=GarbageCollector,name=ConcurrentMarkSweep", target_attr)
 	  	end
 
-	  	def gc_parnew_metrics
-	  		get_metrics("java.lang:type=GarbageCollector,name=ParNew")
+	  	def gc_parnew_metrics(target_attr=[])
+	  		get_metrics("java.lang:type=GarbageCollector,name=ParNew", target_attr)
 	  	end
 
-	  	def os_metrics
-	  		get_metrics("java.lang:type=OperatingSystem")
+	  	def os_metrics(target_attr=[])
+	  		get_metrics("java.lang:type=OperatingSystem", target_attr)
 	  	end
 
-	  	def query_manager_metrics
-	  		get_metrics("com.facebook.presto.execution:name=QueryManager")
+	  	def query_manager_metrics(target_attr=[])
+	  		get_metrics("com.facebook.presto.execution:name=QueryManager", target_attr)
 	  	end
 
-	  	def query_execution_metrics
-	  		get_metrics("com.facebook.presto.execution:name=QueryExecution")
+	  	def query_execution_metrics(target_attr=[])
+	  		get_metrics("com.facebook.presto.execution:name=QueryExecution", target_attr)
 	  	end
 
-	  	def node_scheduler_metrics
-	  		get_metrics("com.facebook.presto.execution:name=NodeScheduler")
+	  	def node_scheduler_metrics(target_attr=[])
+	  		get_metrics("com.facebook.presto.execution:name=NodeScheduler", target_attr)
 	  	end
 
-	  	def task_executor_metrics
-	  		get_metrics("com.facebook.presto.execution:name=TaskExecutor")
+	  	def task_executor_metrics(target_attr=[])
+	  		get_metrics("com.facebook.presto.execution:name=TaskExecutor", target_attr)
 	  	end
 
-	  	def task_manager_metrics
-	  		get_metrics("com.facebook.presto.execution:name=TaskManager")
+	  	def task_manager_metrics(target_attr=[])
+	  		get_metrics("com.facebook.presto.execution:name=TaskManager", target_attr)
 	  	end
 
   	end 
